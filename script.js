1adc733794bb2f4847f615eeef20a124f4cdde45
Magdalena Checinski <madzix.p730@gmail.com>
2020-07-05 20:35:20 +0200

    created tasks list using java script

---
+++ script.js
@@ -0,0 +1,29 @@
+{
+  const tasks = [{
+
+    content: "clean the kitchen",
+    done: false,
+  }, {
+    content: "call to mama",
+    done: false,
+  }];
+
+  const render = () => {
+
+    let htmlText = "";
+
+    for (const task of tasks) {
+      htmlText += ` <li class="section__taskItem"><button class="taskItem__button taskItem__button--doneTask js-done"><i
+      class="fas fa-plus"></i><i class="fas fa-check hide"></i></button>
+  <p>${task.content}</p><button class=" taskItem__button taskItem__button--removeTask js-remove"><i
+      class="fas fa-trash"></i></button>
+</li>`
+
+    };
+    document.querySelector(".js-taskList").innerHTML = htmlText;
+  };
+  const init = () => {
+    render();
+  };
+  init()
+}
