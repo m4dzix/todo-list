78529132a8203936fd0dc501f80cb06ad61c6083
Magdalena Checinski <madzix.p730@gmail.com>
2020-07-05 21:42:46 +0200

    adding new tasks

--- script.js
+++ script.js
@@ -13,17 +13,37 @@
     let htmlText = "";
 
     for (const task of tasks) {
-      htmlText += ` <li class="section__taskItem"><button class="taskItem__button taskItem__button--doneTask js-done"><i
+      htmlText += ` <li     class="section__taskItem"><button class="taskItem__button taskItem__button--doneTask js-done"><i
       class="fas fa-plus"></i><i class="fas fa-check hide"></i></button>
-  <p>${task.content}</p><button class=" taskItem__button taskItem__button--removeTask js-remove"><i
+      <p>${task.content}</p><button class=" taskItem__button taskItem__button--removeTask js-remove"><i
       class="fas fa-trash"></i></button>
-</li>`
+      </li>`
 
     };
     document.querySelector(".js-taskList").innerHTML = htmlText;
   };
+
+  const addNewTask = () => {
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim()
+
+    if (newTaskContent !== "") {
+      tasks.push({
+        content: newTaskContent,
+      });
+      render();
+    }
+  };
+  const onFormSubmit = (event, ) => {
+    event.preventDefault();
+
+    addNewTask()
+  }
+
   const init = () => {
     render();
-  };
+    const form = document.querySelector(".js-form")
+    form.addEventListener("submit", onFormSubmit)
+  }
   init()
 }
