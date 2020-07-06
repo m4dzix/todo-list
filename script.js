58812a99ebb96a57783c65a486bf8b97444ea0fa
Magdalena Checinski <madzix.p730@gmail.com>
2020-07-06 19:51:53 +0200

    removing tasks

--- script.js
+++ script.js
@@ -6,21 +6,36 @@
   }, {
     content: "call to mama",
     done: false,
-  }];
+  }, ];
 
+  const removeTask = (index) => {
+    tasks.splice(index, 1);
+    render();
+  }
   const render = () => {
 
     let htmlText = "";
 
     for (const task of tasks) {
       htmlText += ` <li     class="section__taskItem"><button class="taskItem__button taskItem__button--doneTask js-done"><i
-      class="fas fa-plus"></i><i class="fas fa-check hide"></i></button>
+      class="fas fa-plus "></i><i class="fas fa-check hide"></i></button>
       <p>${task.content}</p><button class=" taskItem__button taskItem__button--removeTask js-remove"><i
       class="fas fa-trash"></i></button>
       </li>`
 
     };
+
     document.querySelector(".js-taskList").innerHTML = htmlText;
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index)
+
+      });
+    });
+
   };
 
   const addNewTask = () => {
@@ -31,19 +46,28 @@
       tasks.push({
         content: newTaskContent,
       });
+
       render();
-    }
+    };
+
   };
-  const onFormSubmit = (event, ) => {
+
+
+  const onFormSubmit = (event) => {
     event.preventDefault();
 
-    addNewTask()
-  }
+    addNewTask();
+
+  };
+
 
   const init = () => {
-    render();
+
+
     const form = document.querySelector(".js-form")
-    form.addEventListener("submit", onFormSubmit)
+    form.addEventListener("submit", onFormSubmit);
+
+    render();
   }
   init()
 }
