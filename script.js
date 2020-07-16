ebb1e333efd82656a19cfd3c1bed167fcac77be6
Magdalena Checinski <madzix.p730@gmail.com>
2020-07-16 17:57:59 +0200

    Removed plus icon inside green botton

--- script.js
+++ script.js
@@ -89,7 +89,7 @@
       htmlText +=
         `<li class="section__taskItem ${task.done && hideDoneTasks ? "section__taskItem--hidden" : ""}">
         <button class="taskItem__button taskItem__button--doneTask js-done">
-    ${task.done ?'<i class="js-checkIcon fas fa-check"></i>': '<i class= "js-plusIcon fas fa-plus"></i>'}
+    ${task.done ?'<i class="js-checkIcon fas fa-check"></i>': ''}
         </button>
         <p class="js-paragraph list__paragraph ${ task.done ? "list__paragraph--done" : "" }" > ${task.content}</p> 
         <button class=" taskItem__button taskItem__button--removeTask js-remove">
